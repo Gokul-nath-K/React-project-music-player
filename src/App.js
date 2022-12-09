8e8d4fe6295dd2b499799c86f9bfe368f32317fd
Gokul-nath-K <gokulnathk003@gmail.com>
2022-12-09 14:23:33 +0530

    Route changed to full page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import './styles.css'
-import Directed from './Directed';
-// import SignIn from './Pages/Sign-in';
-// import {  Switch, Route } from 'react-router-dom'
+import {  Switch, Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage';
+import SearchPage from './Pages/SearchPage';
+import SettingsPage from './Pages/SettingsPage';
+import SignInPage from './Pages/SignInPage';
 
 function App() {
   return (
    <>
-    {/* <Switch>
-      <Route exact path='/'> <Directed /> </Route>
-      <Route path='/signin'> <SignIn /> </Route>
-    </Switch> */}
-    <Directed />
+    <Switch>
+      <Route exact path='/'> <HomePage /> </Route>
+      <Route exact path='/homepage'> <HomePage /> </Route>
+      <Route exact path='/homepage/search'> <SearchPage /> </Route>
+      <Route exact path='/homepage/settings'> <SettingsPage /> </Route>
+      <Route exact path='/signIn'> <SignInPage /> </Route>
+    </Switch>
    </>
   );
 }
--- src/Directed.js
+++ src/Directed.js
@@ -2,22 +2,26 @@ import './App.css';
 import './styles.css';
 import React from 'react'
 import {  Switch, Route } from 'react-router-dom'
-import Search from './Pages/Search';
 import MusicControlBar from './components/MusicControlBar';
 import SideBar from './components/sideBar';
 import Settings from './Pages/settings';
 import Home from './Pages/Home'
+import AppTopBar from './components/AppTopBar';
+import SearchPage from './Pages/SearchPage';
 
 export default function Directed() {
     return (
         <>
+            <div className='app-top-bar'>
+                <AppTopBar />
+            </div>
             <div className='side-nav-bar'>
                 <SideBar />
             </div>
             <div className='content-container'>
                 <Switch>
-                    <Route exact path='/home' > <Home /> </Route>
-                    <Route path='/search' > <Search /> </Route>
+                    <Route path='/home' > <Home /> </Route>
+                    <Route path='/search' > <SearchPage /> </Route>
                     <Route path='/settings' > <Settings /> </Route>
                 </Switch>
             </div>
