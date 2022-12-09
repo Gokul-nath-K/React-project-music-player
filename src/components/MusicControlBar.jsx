054d4e90e93e50093fd6c6b9da8c65898460556b
Gokul-nath-K <gokulnathk003@gmail.com>
2022-12-09 14:26:28 +0530

    Pasuse/play updated using states.

--- src/components/MusicControlBar.jsx
+++ src/components/MusicControlBar.jsx
@@ -1,66 +1,79 @@
 import "../App.css";
-import React, { Component } from "react";
-import { Grid, IconButton } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, Grid, IconButton, makeStyles } from "@material-ui/core";
 import PlayCircleFilled from "@material-ui/icons/PlayCircleFilled";
-import ContinuousSlider from '../MaterialUI/VolumeSlider';
+import ContinuousSlider from "../MaterialUI/VolumeSlider";
 import MediaCard from "../MaterialUI/MusicCard";
 import { withStyles } from "@material-ui/core";
-import Replay10Icon from '@material-ui/icons/Replay10';
-import Forward10Icon from '@material-ui/icons/Forward10';
-import SkipPreviousTwoToneIcon from '@material-ui/icons/SkipPreviousTwoTone';
-import SkipNextTwoToneIcon from '@material-ui/icons/SkipNextTwoTone';
-import QueueMusicRoundedIcon from '@material-ui/icons/QueueMusicRounded';
-import { MuiThemeProvider, unstable_createMuiStrictModeTheme } from '@material-ui/core';
+import Replay10Icon from "@material-ui/icons/Replay10";
+import Forward10Icon from "@material-ui/icons/Forward10";
+import SkipPreviousTwoToneIcon from "@material-ui/icons/SkipPreviousTwoTone";
+import SkipNextTwoToneIcon from "@material-ui/icons/SkipNextTwoTone";
+import QueueMusicRoundedIcon from "@material-ui/icons/QueueMusicRounded";
+import { PauseCircleFilledRounded } from "@material-ui/icons";
 
-const theme = unstable_createMuiStrictModeTheme({
-  typography: {
-    fontSize: 19 ,
-  },
-});
-const useStyles = theme => ({
+const useStyles = makeStyles({
   icon: {
-    color: 'white'
+    color: "#dddddd",
+    fontSize: "140%",
   },
   slider: {
-    color: 'white',
+    color: "white",
+    fontSize: "140%",
   },
 });
 
-class MusicControlBar extends Component {
-  render() {
-    const {classes} = this.props;
-    return (
-      <div >
-        <Grid container className="music-bar" >
-            <Grid 
-            item md={3} xs={2}
-            className="centerColumn"
-            >
-                {<MediaCard />}
-            </Grid>
-            <MuiThemeProvider theme={theme}>
-            <Grid
-            item md={6} xs={2}
-            className="centerColumn"
-            >
-                <IconButton  className={classes.icon} ><Replay10Icon /></IconButton>
-                <IconButton  className={classes.icon} ><SkipPreviousTwoToneIcon /></IconButton>
-                <IconButton  className={classes.icon} ><PlayCircleFilled/></IconButton>
-                <IconButton  className={classes.icon} ><SkipNextTwoToneIcon/></IconButton>
-                <IconButton  className={classes.icon} ><Forward10Icon/></IconButton> 
-            </Grid>
-            <Grid 
-            item md={3} xs={2}
-            className="centerColumn"
-              >
-                <IconButton  className={classes.icon} ><QueueMusicRoundedIcon/></IconButton> 
-                <IconButton  className={classes.slider} ><ContinuousSlider/></IconButton> 
-            </Grid>
-            </MuiThemeProvider>
-        </Grid>
-      </ div>
-    );
-  }
+function MusicControlBar() {
+  const [isPlaying, setPauseClicked] = useState(false);
+
+  const handleClick = () => {
+    setPauseClicked(!isPlaying);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className="music-bar-inner-container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="controls">{<MediaCard />}</div>
+          </div>
+          <div className="col-md-4">
+            <div className="controls">
+              <IconButton className={classes.icon}>
+                <Replay10Icon className={classes.icon} />
+              </IconButton>
+              <IconButton className={classes.icon}>
+                <SkipPreviousTwoToneIcon className={classes.icon} />
+              </IconButton>
+              <IconButton className={classes.icon} onClick={handleClick}>
+                {isPlaying ? (
+                  <PauseCircleFilledRounded className={classes.icon} />
+                ) : (
+                  <PlayCircleFilled className={classes.icon} />
+                )}
+              </IconButton>
+              <IconButton className={classes.icon}>
+                <SkipNextTwoToneIcon className={classes.icon} />
+              </IconButton>
+              <IconButton className={classes.icon}>
+                <Forward10Icon className={classes.icon} />
+              </IconButton>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <IconButton className={classes.icon}>
+              <QueueMusicRoundedIcon className={classes.icon} />
+            </IconButton>
+            <Button>
+              <ContinuousSlider className={classes.icon} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default withStyles(useStyles) (MusicControlBar);
+export default MusicControlBar;
